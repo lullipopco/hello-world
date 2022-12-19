7791dda5fa9ceb08851808f125eb0b374a8d6091
Jonathan M. James <jjames@lullipop.com>
2022-12-18 20:38:04 -0500

    .

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+  <div>
+    <h1>Hello World</h1>
+    <p>Wlcome to my first app</p>
+    <p>This is some filler text for me to see</p>
+    </div>
+  );
 }
